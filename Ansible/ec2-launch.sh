b8e37955d399fd4a63205e30601070848c5b821a
shuja <shuja.devops@gmail.com>
2021-12-18 22:56:26 +0530

    ec2 spot instance from launch template

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -3,4 +3,8 @@
 #1. Create a Spot Instance
 #2. Take take instance IP and register in DNS
 
-aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+#aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification #file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,#Value=frontend}]" | jq
+TEMP_ID="lt-06e98f843b7024a45"
+TEMP_VER=3
+
+aws ec2 run-instances --launch-template LaunchTemplateId=$(TEMP_ID),Version=$(TEMP_VER)
