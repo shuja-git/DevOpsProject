c137c64996ecde975802188157cf8a43bf25cfbc
shuja <shuja.devops@gmail.com>
2021-12-18 23:25:17 +0530

    ec2 spot instance from launch template

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -6,6 +6,6 @@
 #aws ec2 request-spot-instances --instance-count 1 --type "persistent" --launch-specification #file://spot.json --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,#Value=frontend}]" | jq
 #TEMP_ID="lt-06e98f843b7024a45"
 #TEMP_VER=3
-TEMP_ID="lt-08d5aa9e8bc287ac7"
-TEMP_VER=1
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
+ID="lt-08d5aa9e8bc287ac7"
+VER=1
+aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} | jq
