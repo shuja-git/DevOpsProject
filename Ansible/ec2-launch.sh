3bf3940678bc47ce880646cdd493b4c8029cf3d8
shuja <shuja.devops@gmail.com>
2021-12-19 11:20:07 +0530

    hardcoded frontend replaced

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -3,9 +3,16 @@
 #1. Create a Spot Instance
 #2. Take take instance IP and register in DNS
 
+if [ -z "$1" ]; then
+  echo "Input is missing"
+  exit 1
+fi
+
+COMPONENT=$1
+
 # check if instance is already there
 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance is already there"
   exit
@@ -14,5 +21,5 @@ fi
 
 ID="lt-08d5aa9e8bc287ac7"
 VER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" | jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
