6bc16e6bc34034a97e38677709ecaab60e7dbb7c
shuja <shuja.devops@gmail.com>
2021-12-19 15:11:26 +0530

    Merge remote-tracking branch 'origin/main'

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -24,9 +24,9 @@ VER=2
 ZONE_ID=Z078908237A6SF5H7OAQ8
 aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
-
-#Update the DNS Record
-sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id ZONE_ID --change-batch file:///tmp/record.json | jq
+#IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
+#
+##Update the DNS Record
+#sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+#aws route53 change-resource-record-sets --hosted-zone-id ZONE_ID --change-batch file:///tmp/record.json | jq
 
