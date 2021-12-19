047de97659ba7da4637760d0dab945bb54e49112
shuja <shuja.devops@gmail.com>
2021-12-19 16:06:16 +0530

    update dns

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -10,21 +10,23 @@ fi
 
 COMPONENT=$1
 
+
+ID="lt-08d5aa9e8bc287ac7"
+VER=2
+ZONE_ID=Z078908237A6SF5H7OAQ8
+
 # check if instance is already there
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq 0 ]; then
   echo -e "\e[1;33mInstance is already there\e[0m"
-  exit
+else
+aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 fi
 
 
-ID="lt-08d5aa9e8bc287ac7"
-VER=2
-ZONE_ID=Z078908237A6SF5H7OAQ8
-aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
-IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
 
 ##Update the DNS Record
 sed -e "s/IP/${IP}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
