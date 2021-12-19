254b01dbafeb6e7b939218a8213424945a9ae287
shuja <shuja.devops@gmail.com>
2021-12-19 13:13:42 +0530

    update DNS Record

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -27,5 +27,6 @@ aws ec2 run-instances --launch-template LaunchTemplateId=${ID},Version=${VER} --
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g')
 
 #Update the DNS Record
-#sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
-#aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ZONE_ID --change-batch file:///tmp/record.json | jq
+
