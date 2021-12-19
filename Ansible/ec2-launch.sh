ec7f8fa356766928f2f8996559a48a40001004ce
shuja <shuja.devops@gmail.com>
2021-12-19 11:24:08 +0530

    hardcoded frontend replaced

--- Ansible/ec2-launch.sh
+++ Ansible/ec2-launch.sh
@@ -4,7 +4,7 @@
 #2. Take take instance IP and register in DNS
 
 if [ -z "$1" ]; then
-  echo "Input is missing"
+  echo -e "\e[1;31mInput is missing\e[0m"
   exit 1
 fi
 
@@ -14,7 +14,7 @@ COMPONENT=$1
 
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 if [ $? -eq 0 ]; then
-  echo "Instance is already there"
+  echo -e "\e[1;33mInstance is already there\e[0m"
   exit
 fi
 
